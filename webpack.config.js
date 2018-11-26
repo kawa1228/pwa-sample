a55dca698b3e40cd2b370df5dc0577d824e73317
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-11-26 22:00:29 +0900

    first

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const { GenerateSW } = require("workbox-webpack-plugin");
+module.exports = {
+  plugins: [
+    new GenerateSW({
+      swDest: "sw.js",
+      skipWaiting: true,
+      clientsClaim: true
+    })
+  ]
+};
